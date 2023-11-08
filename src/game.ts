617a414f782f94e0b0bd3ca0370f60d6a70ee250
o-safonchik <safonchik@protonmail.com>
2023-11-09 02:30:50 +0600

    1

--- src/game.ts
+++ src/game.ts
@@ -9,20 +9,16 @@ export const game: {
     "toStep": (step: number) => boolean,
     "checkStatus": () => string
 } = {
-    // TODO
-    // Необходимо инициализировать steps массивом с пустой доской
+    // steps инициализируется массивом с пустой доской
     steps: [["_", "_", "_", "_", "_", "_", "_", "_", "_"]], 
     current: 0,
     move: function (index: number): boolean {
-        // TODO
         // Вызывает checkStatus, и, если игра окончена, возвращает false
         if(this.checkStatus() != "Идет игра") return false
         // Определяет, какой символ ходит, и пытается сделать ход 
         //  с помощью board.move.
         // Если ход можно сделать, то добавляет новыу позицию в steps, 
         //  обновляет current и возвращает true, иначе возвращает false
-        // Нужно учесть, что если вызывалась функция toStep, то 
-        //  current можно указывать не на последний элемент steps
         if(this.current != this.steps.length-1){
             this.steps = this.steps.slice(0, this.current+1)
         }
@@ -43,21 +39,18 @@ export const game: {
             }
             else return false
         }
-        //console.log(this.steps.length)
-        //console.log(this.current)
-        console.log(this.steps)
         return true
-        
     },
+
+    //Функция перехода к определённому ходу
     toStep: function (step: number): boolean {
-        //console.log(this.steps.length)
-        //console.log(this.steps)
         if ((this.steps.length-1) < step) return false // Проверяет, что в steps есть элемент с индексом step, если нет то возвращает false
         this.current = step // Делает current равным step и обновляет свойство cell в board
         board.cells = [...this.steps[step]]
         return true
     },
 
+    //Функция проверки статуса игры
     checkStatus: function (): string {
         // Возвращает "Ничья", "Победил 0", "Победил X" или "Идет игра"
         //  в зависимости от ситуации на доске
